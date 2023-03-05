70c60d8ef5f19da633da6c31ddf8670ea506a8bd
ahmedasaad12 <119532372+ahmedasaad12@users.noreply.github.com>
2023-03-05 21:04:52 +0200

    Update scribtmovie.js

--- home/scribtmovie.js
+++ home/scribtmovie.js
@@ -2,7 +2,7 @@ let request = new XMLHttpRequest();
 
 
 
-request.open("GET" ,"https://api.themoviedb.org/3/trending/movie/week?api_key=3f3bdbaa5120d13e9d99e747c1448b0c");
+request.open("GET" ,"https://api.themoviedb.org/3/trending/movie/day?api_key=d903bcdae97d41411a8e54f49fb3b5d0");
 
 
 request.send();
@@ -20,7 +20,7 @@ request.addEventListener("load" , function(){
         <div class="col-lg-3 col-md-3 col-sm-12 ">
         <div>
         <h6 class="text-white">${photo[i].vote_average}</h6>
-        <img class="img-fluid mt-5" src="https://image.tmdb.org/t/p/w500${photo[i].backdrop_path}" alt="">
+        <img class="img-fluid mt-5" src="https://image.tmdb.org/t/p/w500${photo[i].poster_path}" alt="">
     </div></div>
         `;
         console.log(photo[i]);
